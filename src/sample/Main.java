142fed92a7a8701dc1933ea4c0be1fcdcd06c6f8
Thuan <s3877024@rmit.edu.vn>
2021-07-17 18:30:53 +0700

    fix

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Testing branch");
-        // coding
+        // thia thuan
         primaryStage.show();
     }
 
