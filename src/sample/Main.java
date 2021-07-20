bf96540a80d82327f4784e994b69ff7ccc9e1e1b
Tommy <s3879362@rmit.edu.vn>
2021-07-20 21:54:45 +0700

    Convert to SceneBuilder

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,36 +1,19 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        BorderPane borderPane = new BorderPane();
-        borderPane.setStyle("-fx-background-color: white;");
-
-        // set the template title for the main page
-        // use the custom class for adding to all other scene
-        TopPane topPane = new TopPane(borderPane);
-        borderPane.setTop(topPane);
-
-        // adding developer name at the bottom
-        borderPane.setBottom(new Footer());
-
-        // add important news to the center
-        borderPane.setCenter(new ImportantNews(borderPane));
-
-        // add pane to scene and show
-        Scene scene = new Scene(borderPane, 1500, 1300);
-        primaryStage.setTitle("Demo Web Page");
-        primaryStage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 1500, 1300));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
