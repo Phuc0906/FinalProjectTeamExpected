eb14117ed8bc7401715080c5258acbb161adda72
Anh <s3877798@rmit.edu.vn>
2021-07-21 17:02:07 +0700

    asd

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start (Stage primaryStage) throws Exception{
+        Parent homePage = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+        primaryStage.setTitle("Projekt Red");
+        primaryStage.setScene(new Scene(homePage,1000, 720));
         primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
