252ffe110e1ddbe8c82153cf35c414608bf5e3ae
Tommy <s3879362@rmit.edu.vn>
2021-07-22 22:44:36 +0700

    adding label clicking

--- src/NewsApp/Controller.java
+++ src/NewsApp/Controller.java
@@ -2,13 +2,26 @@ package NewsApp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Controller {
+
+    String demoNewspaper = "grwhawerbeabdcvbeiaufbeibveiyuffkehfuidgfiegfSDBFUEGFUIEF";
 
     @FXML
     public Button btnCovid, btnNews, btnHome;
@@ -66,4 +79,15 @@ public class Controller {
         relatedNews5.setText("RelatedNews 5");
     }
 
+    @FXML
+    public VBox impNews;
+    public void clickImportantNews(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLFile/ImportantNewsPage.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) impNews.getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Hello");
+        stage.show();
+    }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,6 @@ module FinalProjectTeamExpected {
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.compiler;
     requires org.jsoup;
 
     opens NewsApp;
