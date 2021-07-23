89d4e7a91c2adc9bf3a98b64faffeb5d658166cd
Anh <s3877798@rmit.edu.vn>
2021-07-23 16:09:58 +0700

    23/7

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 
@@ -15,5 +18,17 @@ public class Main extends Application {
         primaryStage.setTitle("Projekt Red");
         primaryStage.setScene(new Scene(homePage,1000, 720));
         primaryStage.show();
+        primaryStage.setOnCloseRequest(event -> closeApp(primaryStage));
+    }
+
+    public void closeApp(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Close App");
+        alert.setContentText("Do you want to close the app?");
+        alert.setHeaderText("You are trying to close the app");
+
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            stage.close();
+        }
     }
 }
--- src/sample/PageController.java
+++ src/sample/PageController.java
@@ -1,12 +1,18 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import javax.swing.event.AncestorEvent;
 import java.io.IOException;
 
 public class PageController {
@@ -38,5 +44,26 @@ public class PageController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    AnchorPane mainPane;
+    public void closeApp(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Close App");
+        alert.setContentText("Do you want to close the app?");
+        alert.setHeaderText("You are trying to close the app");
+
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) mainPane.getScene().getWindow();
+            stage.close();
+        }
+    }
+
+    @FXML
+    private TextField searchField;
+
+    public void search(ActionEvent actionEvent) {
+        System.out.printf(searchField.getText());
     }
 }
