1b16bc763d381ea62a1bc365eb0f21861e7cb14f
Thuan <s3877024@rmit.edu.vn>
2021-07-25 18:20:29 +0700

    splash screen

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,25 +1,52 @@
 package sample;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
 
 public class Main extends Application {
 
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-          Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-          Scene scene = new Scene(root);
-          scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-          primaryStage.setScene(scene);
-          primaryStage.setTitle("CSS showed");
-          primaryStage.show();
-    }
+    public void start (Stage primaryStage) throws Exception{
+        BorderPane pane = FXMLLoader.load(getClass().getResource("pageFXML/SplashScreen.fxml"));
+        primaryStage.setScene(new Scene(pane, 1000, 720));
+        primaryStage.show();
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(3), pane);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+        fadeIn.setCycleCount(1);
 
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(3), pane);
+        fadeOut.setFromValue(1);
+        fadeOut.setToValue(0);
+        fadeOut.setCycleCount(1);
+        fadeIn.play();
 
-    public static void main(String[] args) {
-        launch(args);
+        fadeIn.setOnFinished(e -> {
+            fadeOut.play();
+        });
+        fadeOut.setOnFinished(e -> {
+            Parent homePage = null;
+            try {
+                homePage = FXMLLoader.load(getClass().getResource("pageFXML/HomePage.fxml"));
+                primaryStage.setTitle("Projekt Red");
+                primaryStage.setScene(new Scene(homePage,1000, 720));
+                primaryStage.show();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
     }
 }
