977974a16ed18ad2261353b06cac8f46f0b5d961
Anh <s3877798@rmit.edu.vn>
2021-08-10 14:30:46 +0700

    finish

--- src/sample/WorldController.java
+++ src/sample/WorldController.java
@@ -32,6 +32,9 @@ public class WorldController {
         this.worldNews.loadTuoiTre("https://tuoitre.vn/the-gioi.htm");
         this.worldNews.loadThanhNien("https://thanhnien.vn/the-gioi/");
         this.worldNews.loadNhanDan("https://nhandan.vn/thegioi");
+        this.worldNews.loadNhanDan("https://nhandan.vn/xahoi");
+        this.worldNews.loadNhanDan("https://nhandan.vn/phapluat");
+        this.worldNews.loadNhanDan("https://nhandan.vn/thethao");
     }
 
 
@@ -130,6 +133,7 @@ public class WorldController {
         stage.show();
     }
 
+//    Set Img methods block
     @FXML
     ArrayList<ImageView> imgList;
     public void setImage() {
@@ -137,6 +141,35 @@ public class WorldController {
             imgList.get(i).setImage(new Image(worldNews.getNews(i).getImageURL()));
     }
 
+    @FXML
+    ArrayList<ImageView> imgList2;
+    public void setImage2() {
+        for (int i = 0; i < imgList2.size(); i++)
+            imgList2.get(i).setImage(new Image(worldNews.getNews(10 + i).getImageURL()));
+    }
+
+    @FXML
+    ArrayList<ImageView> imgList3;
+    public void setImage3() {
+        for (int i = 0; i < imgList3.size(); i++)
+            imgList3.get(i).setImage(new Image(worldNews.getNews(20 + i).getImageURL()));
+    }
+
+    @FXML
+    ArrayList<ImageView> imgList4;
+    public void setImage4() {
+        for (int i = 0; i < imgList4.size(); i++)
+            imgList4.get(i).setImage(new Image(worldNews.getNews(30 + i).getImageURL()));
+    }
+
+    @FXML
+    ArrayList<ImageView> imgList5;
+    public void setImage5() {
+        for (int i = 0; i < imgList5.size(); i++)
+            imgList5.get(i).setImage(new Image(worldNews.getNews(40 + i).getImageURL()));
+    }
+
+//    Set title methods block
     @FXML
     ArrayList<Label> titleList;
     public void setTitle() {
@@ -149,6 +182,52 @@ public class WorldController {
     }
 
     @FXML
+    ArrayList<Label> titleList2;
+    public void setTitle2() {
+
+        for (int i = 0; i < titleList2.size(); i++) {
+            titleList2.get(i).setFont(Font.font("Time New Roman", FontWeight.BOLD, 30));
+            titleList2.get(i).setAlignment(Pos.TOP_LEFT);
+            titleList2.get(i).setText(worldNews.getNews(10 + i).getTitle());
+        }
+    }
+
+    @FXML
+    ArrayList<Label> titleList3;
+    public void setTitle3() {
+
+        for (int i = 0; i < titleList3.size(); i++) {
+            titleList3.get(i).setFont(Font.font("Time New Roman", FontWeight.BOLD, 30));
+            titleList3.get(i).setAlignment(Pos.TOP_LEFT);
+            titleList3.get(i).setText(worldNews.getNews(20 + i).getTitle());
+        }
+    }
+
+    @FXML
+    ArrayList<Label> titleList4;
+    public void setTitle4() {
+
+        for (int i = 0; i < titleList4.size(); i++) {
+            titleList4.get(i).setFont(Font.font("Time New Roman", FontWeight.BOLD, 30));
+            titleList4.get(i).setAlignment(Pos.TOP_LEFT);
+            titleList4.get(i).setText(worldNews.getNews(30 + i).getTitle());
+        }
+    }
+
+    @FXML
+    ArrayList<Label> titleList5;
+    public void setTitle5() {
+
+        for (int i = 0; i < titleList5.size(); i++) {
+            titleList5.get(i).setFont(Font.font("Time New Roman", FontWeight.BOLD, 30));
+            titleList5.get(i).setAlignment(Pos.TOP_LEFT);
+            titleList5.get(i).setText(worldNews.getNews(40 + i).getTitle());
+        }
+    }
+
+//    Set description methods block
+
+    @FXML
     ArrayList<Label> descriptionList;
     public void setDescription() {
         for (int i = 0; i < descriptionList.size(); i++) {
@@ -156,5 +235,102 @@ public class WorldController {
             descriptionList.get(i).setAlignment(Pos.TOP_LEFT);
             descriptionList.get(i).setText(new SupportedMethod().breakingString(worldNews.getNews(i).getDescription(), 15));
         }
+    }
+
+    @FXML
+    ArrayList<Label> descriptionList2;
+    public void setDescription2() {
+        for (int i = 0; i < descriptionList2.size(); i++) {
+            descriptionList2.get(i).setFont(Font.font("Time New Roman", FontWeight.BOLD, 15));
+            descriptionList2.get(i).setAlignment(Pos.TOP_LEFT);
+            descriptionList2.get(i).setText(new SupportedMethod().breakingString(worldNews.getNews(10 + i).getDescription(), 15));
+        }
+    }
+
+    @FXML
+    ArrayList<Label> descriptionList3;
+    public void setDescription3() {
+        for (int i = 0; i < descriptionList3.size(); i++) {
+            descriptionList3.get(i).setFont(Font.font("Time New Roman", FontWeight.BOLD, 15));
+            descriptionList3.get(i).setAlignment(Pos.TOP_LEFT);
+            descriptionList3.get(i).setText(new SupportedMethod().breakingString(worldNews.getNews(20 + i).getDescription(), 15));
+        }
+    }
+
+    @FXML
+    ArrayList<Label> descriptionList4;
+    public void setDescription4() {
+        for (int i = 0; i < descriptionList4.size(); i++) {
+            descriptionList4.get(i).setFont(Font.font("Time New Roman", FontWeight.BOLD, 15));
+            descriptionList4.get(i).setAlignment(Pos.TOP_LEFT);
+            descriptionList4.get(i).setText(new SupportedMethod().breakingString(worldNews.getNews(30 + i).getDescription(), 15));
+        }
+    }
+
+    @FXML
+    ArrayList<Label> descriptionList5;
+    public void setDescription5() {
+        for (int i = 0; i < descriptionList5.size(); i++) {
+            descriptionList5.get(i).setFont(Font.font("Time New Roman", FontWeight.BOLD, 15));
+            descriptionList5.get(i).setAlignment(Pos.TOP_LEFT);
+            descriptionList5.get(i).setText(new SupportedMethod().breakingString(worldNews.getNews(40 + i).getDescription(), 15));
+        }
+    }
+
+//   ToPage methods block
+    public void toWorldPage2(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("pageFXML/WorldPage2.fxml"));
+        root = loader.load();
+        WorldController worldController = loader.getController();
+        worldController.setImage2();
+        worldController.setTitle2();
+        worldController.setDescription2();
+
+        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void toWorldPage3(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("pageFXML/WorldPage3.fxml"));
+        root = loader.load();
+        WorldController worldController = loader.getController();
+        worldController.setImage3();
+        worldController.setTitle3();
+        worldController.setDescription3();
+
+        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void toWorldPage4(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("pageFXML/WorldPage4.fxml"));
+        root = loader.load();
+        WorldController worldController = loader.getController();
+        worldController.setImage4();
+        worldController.setTitle4();
+        worldController.setDescription4();
+
+        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void toWorldPage5(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("pageFXML/WorldPage5.fxml"));
+        root = loader.load();
+        WorldController worldController = loader.getController();
+        worldController.setImage5();
+        worldController.setTitle5();
+        worldController.setDescription5();
+
+        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
