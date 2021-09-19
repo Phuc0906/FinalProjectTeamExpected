3a0ebccf1c2475f8b6a825fc159247a72d8f1440
PhongLe <s3877819@rmit.edu.vn>
2021-09-19 21:52:35 +0700

    Add infomation

--- src/sample/LoadingScreenController.java
+++ src/sample/LoadingScreenController.java
@@ -1,3 +1,23 @@
+/*
+  RMIT University Vietnam
+  Course: INTE2512 Object-Oriented Programming
+  Semester: 2021B
+  Assessment: Final Project
+  Created  date: 17/7/2021
+  Author: Nguyen Hung Anh s3877798
+    Hoang Phuc s3879362
+    Le Tan Phong s3877819
+    Thai Thuan s3877024
+  Last modified date: 19/9/2021
+  Acknowledgement:
+  https://www.youtube.com/watch?v=9XJicRt_FaI&t=5536s
+  https://youtu.be/f06uUtkmtDE
+  https://youtu.be/o-lAsVuskKI
+  https://www.tutorialspoint.com/java/index.htm
+  http://tutorials.jenkov.com/javafx/index.html
+*/
+
+
 package sample;
 
 import javafx.application.Platform;
--- src/sample/WorldController.java
+++ src/sample/WorldController.java
@@ -8,9 +8,15 @@
     Hoang Phuc s3879362
     Le Tan Phong s3877819
     Thai Thuan s3877024
-  Last modified date: 11/9/2021
-  Acknowledgement: Thanks and give credits to the resources that you used in this file
+  Last modified date: 19/9/2021
+  Acknowledgement:
+  https://www.youtube.com/watch?v=9XJicRt_FaI&t=5536s
+  https://youtu.be/f06uUtkmtDE
+  https://youtu.be/o-lAsVuskKI
+  https://www.tutorialspoint.com/java/index.htm
+  http://tutorials.jenkov.com/javafx/index.html
 */
+
 package sample;
 
 
